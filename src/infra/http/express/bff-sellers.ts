3b2e24167cb11c92239cf021cd4bd87891613de5
Vinicius Carvalho <vinicius.carvalho@houpa.app>
2023-03-09 08:57:41 -0300

    feat: add bff sellers

---
+++ src/infra/http/express/bff-sellers.ts
@@ -0,0 +1,25 @@
+import express, { Express, Request, Response } from 'express';
+import { CreateSellerUseCase } from '../../../application/usecases/sellers';
+import { SellerInMemoryRepository } from '../../database/repositories/in-memoy';
+
+const app: Express = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3331;
+const repository = new SellerInMemoryRepository();
+
+app.post('/sellers', async (req: Request, res: Response) => {
+  const { seller } = req.body;
+  if(!seller) {
+    res.status(200).json();
+    return;
+  }
+  const createSellerUseCase = new CreateSellerUseCase(repository);
+  const output = await createSellerUseCase.execute(seller);
+  res.status(201).json(output);
+})
+
+app.listen(port, () => {
+  console.log(`server runnning on ${port}`)
+});
